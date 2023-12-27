29ee5aa4649b1ce2b3f9e24d50ff23a2c5942a4d
Fernando Cuatro <fernandocuatrorivera@gmail.com>
2023-12-27 00:11:16 -0600

    Genera un select por anio

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -1,13 +1,24 @@
 // variables
 const resultado = document.querySelector('#resultado');
+const year      = document.querySelector('#year');
+
+const max = new Date().getFullYear(); // trae el anio actual
+const min = max - 10;
 
 // eventos
 document.addEventListener('DOMContentLoaded', () => {
     // una vez que se complete la carga la pagina, mostramos los autos
     mostrarAutos();
+
+    // llena las opciones de años
+    llenarSelect();
 });
 
 // funciones
+
+/**
+ * muestra los datos recorriendo con un foreach
+ */
 function mostrarAutos() {
     // iteramos sobre autros, es decir sobre el archivo autos
     autos.forEach(auto => {
@@ -19,10 +30,22 @@ function mostrarAutos() {
             ${marca} ${modelo} - (${year}) - $${precio} - ${puertas} Puertas - ${color} - ${transmision}
         `;
 
-
-
-
         // insertar en el HTML
         resultado.appendChild(autoHTML);
     });
+}
+
+
+/**
+ * genera los años del select
+ */
+function llenarSelect() {
+    // generamos loa anios
+    for (let i = max; i >= min; i--) {
+        const opcion = document.createElement('option');
+        opcion.value = i;
+
+        opcion.textContent = i;
+        year.appendChild(opcion); // agrega las opciones de anio al select
+    }
 }
