707745c49d30677a8d61b38c952697f9d66a872c
Fernando Cuatro <fernandocuatrorivera@gmail.com>
2024-01-02 22:39:16 -0600

    Creando objeto tipo objeto

---
+++ 22-Prototypes/js/01-app.js
@@ -0,0 +1,17 @@
+// un prototype estan relacionados con los objetos
+// todos los objetos tienen un prototype
+
+// un objeto
+// el object literal
+const cliente = {
+    nombre: 'Fernando',
+    saldo: 500
+}
+
+// object constructor
+function Cliente(nombre, saldo) {
+    this.nombre = nombre;
+    this.saldo = saldo;
+}
+
+const ferb = new Cliente('Fernando', 500);
