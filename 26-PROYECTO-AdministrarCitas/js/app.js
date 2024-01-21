4035a112566bd9e721b33a6be20005b66ef8b3b9
Fernando Cuatro <fernandocuatrorivera@gmail.com>
2024-01-20 22:57:49 -0600

    Agregando variables, objetos y funciones

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -1,4 +1,4 @@
-// variables
+// variables desde campos del formulario
 const mascotaInput     = document.querySelector('#mascota');
 const propietarioInput = document.querySelector('#propietario');
 const telefonoInput    = document.querySelector('#telefono');
@@ -6,6 +6,7 @@ const fechaInput       = document.querySelector('#fecha');
 const horaInput        = document.querySelector('#hora');
 const sintomasInput    = document.querySelector('#sintomas');
 
+// user interface UI
 const formulario       = document.querySelector('#nueva-cita');
 const contedorCitas    = document.querySelector('#citas');
 
@@ -15,8 +16,29 @@ eventListeners();
 // funciones 
 function eventListeners() {
     // leemos lo que el usuario escribe
-    mascotaInput.addEventListener('change', datosCita);
+    mascotaInput.addEventListener('input', datosCita);
+    propietarioInput.addEventListener('input', datosCita);
+    telefonoInput.addEventListener('input', datosCita);
+    fechaInput.addEventListener('input', datosCita);
+    horaInput.addEventListener('input', datosCita);
+    sintomasInput.addEventListener('input', datosCita);
 }
 
+// un objeto para guardar el textazo del la informacion de la cita
+const citaObj = {
+    mascota    : '',
+    propietario: '',
+    telefono   : '',
+    fecha      : '',
+    hora       : '',
+    sintomas   : ''
+}
+
+// para leer lo que el usuario esta escribiendo 
+// agregamos datos al objeto de citas
+function datosCita(e) {
+    citaObj[e.target.name] = e.target.value;
+    // console.log(citaObj);
 
 
+}
