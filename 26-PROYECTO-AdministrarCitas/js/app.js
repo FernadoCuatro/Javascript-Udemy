a770b3858eafd809eaaf79aaaccc639b8d68e2e2
Fernando Cuatro <fernandocuatrorivera@gmail.com>
2024-02-03 20:53:24 -0600

    Pintamos la listas de citas en el HTML

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -6,6 +6,9 @@ const fechaInput       = document.querySelector('#fecha');
 const horaInput        = document.querySelector('#hora');
 const sintomasInput    = document.querySelector('#sintomas');
 
+// Contenedor para las citas
+const contenedorCitas = document.querySelector('#citas');
+
 // user interface UI
 const formulario       = document.querySelector('#nueva-cita');
 const contedorCitas    = document.querySelector('#citas');
@@ -44,14 +47,84 @@ class UI {
         document.querySelector('#contenido').insertBefore(divMensaje, document.querySelector('.agregar-cita'));
 
         // Quitar el alert despues de 3 segundos
-        setTimeout(() => {
+        setTimeout(() => {  
             divMensaje.remove();
         }, 3000);
+    }
+
+    imprimirCitas({ citas }) {
+        // Se puede aplicar destructuring desde la función...
+        // extrae las citas del objetos con el ({ })
+
+        this.limpiarHTML();
+
+        // accedemos a cada cita
+        citas.forEach(cita => {
+            // aplicamos el destructuring para cada variable junto con el ID
+            const { mascota, propietario, telefono, fecha, hora, sintomas, id } = cita;
+
+            // creamos un div
+            const divCita = document.createElement('div');
+            divCita.classList.add('cita', 'p-3'); // agregamos clases
+            divCita.dataset.id = id; // establecemos el dataset con el ID de la clase extraido desde el destructuring
+
+            // puro scripting para todos los elementos, un textazo
+            const mascotaParrafo = document.createElement('h2');
+            mascotaParrafo.classList.add('card-title', 'font-weight-bolder');
+            mascotaParrafo.innerHTML = `${mascota}`; // imprimimos el nombre de la mascota
+
+            // agregamos los parrafos al div de la cita divCita
+            const propietarioParrafo = document.createElement('p');
+            propietarioParrafo.innerHTML = `<span class="font-weight-bolder">Propietario: </span> ${propietario}`;
+
+            const telefonoParrafo = document.createElement('p');
+            telefonoParrafo.innerHTML = `<span class="font-weight-bolder">Teléfono: </span> ${telefono}`;
+
+            const fechaParrafo = document.createElement('p');
+            fechaParrafo.innerHTML = `<span class="font-weight-bolder">Fecha: </span> ${fecha}`;
+
+            const horaParrafo = document.createElement('p');
+            horaParrafo.innerHTML = `<span class="font-weight-bolder">Hora: </span> ${hora}`;
+
+            const sintomasParrafo = document.createElement('p');
+            sintomasParrafo.innerHTML = `<span class="font-weight-bolder">Síntomas: </span> ${sintomas}`;
+
+            // Agregar un botón de eliminar...
+            const btnEliminar = document.createElement('button');
+            btnEliminar.onclick = () => eliminarCita(id); // añade la opción de eliminar
+            btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
+            btnEliminar.innerHTML = 'Eliminar <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+
+            // Añade un botón de editar...
+            const btnEditar = document.createElement('button');
+            btnEditar.onclick = () => cargarEdicion(cita);
+
+            btnEditar.classList.add('btn', 'btn-info');
+            btnEditar.innerHTML = 'Editar <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>'
+
+            // Agregar al HTML
+            divCita.appendChild(mascotaParrafo);
+            divCita.appendChild(propietarioParrafo);
+            divCita.appendChild(telefonoParrafo);
+            divCita.appendChild(fechaParrafo);
+            divCita.appendChild(horaParrafo);
+            divCita.appendChild(sintomasParrafo);
+            divCita.appendChild(btnEliminar)
+            divCita.appendChild(btnEditar)
+
+            contenedorCitas.appendChild(divCita);
+        });
+    }
+
+    limpiarHTML() {
+        while (contenedorCitas.firstChild) {
+            contenedorCitas.removeChild(contenedorCitas.firstChild);
+        }
     }
 }
 
 // instancias de las clases
-const UI = new UI();
+const ui = new UI();
 const administrarCitas = new Citas();
 
 // eventos
@@ -102,14 +175,14 @@ function nuevaCita(e) {
         return;
     }
 
-    // generar un id unico
+    // generar un id único
     citaObj.id = Date.now();
 
     // creando una nueva cita
     // console.log(citaObj);
 
     // Añade la nueva cita
-    administrarCitas.agregarCita({...citaObj}); // para que se mande una copia 
+    administrarCitas.agregarCita( {...citaObj} ); // para que se mande una copia 
 
     // Mostrar mensaje de que todo esta bien...
     // ui.imprimirAlerta('Se agregó correctamente')
@@ -119,6 +192,9 @@ function nuevaCita(e) {
 
     // Reinicia el objeto para evitar futuros problemas de validación
     reiniciarObjeto();
+
+    // Imprimir el HTML de citas
+    ui.imprimirCitas(administrarCitas);
 
 }
 
@@ -131,38 +207,3 @@ function reiniciarObjeto() {
     citaObj.hora        = '';
     citaObj.sintomas    = '';
 }
-
-// function nuevaCita(e) {
-
-
-
-//     if(editando) {
-//         // Estamos editando
-//         administrarCitas.editarCita( {...citaObj} );
-
-//         ui.imprimirAlerta('Guardado Correctamente');
-
-//         formulario.querySelector('button[type="submit"]').textContent = 'Crear Cita';
-
-//         editando = false;
-
-//     } else {
-//         // Nuevo Registrando
-
-//         // Generar un ID único
-//         citaObj.id = Date.now();
-        
-//         // Añade la nueva cita
-//         administrarCitas.agregarCita({...citaObj});
-
-//         // Mostrar mensaje de que todo esta bien...
-//         ui.imprimirAlerta('Se agregó correctamente')
-//     }
-
-
-//     // Imprimir el HTML de citas
-//     ui.imprimirCitas(administrarCitas);
-
-
-
-// }
