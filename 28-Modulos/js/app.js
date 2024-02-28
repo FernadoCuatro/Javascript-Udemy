d61da5fadd3dedfae26cdf3d70a827e660c3eb22
Fernando Cuatro <fernandocuatrorivera@gmail.com>
2024-02-27 22:29:11 -0600

    Modulos en ES6

---
+++ 28-Modulos/js/app.js
@@ -0,0 +1,14 @@
+// IIFE es una funcion que se ejecuta inmediatamente se carga la pagina
+// (function() {
+//     // mantiene las variables localmente no se mezclan con otros archivos
+//     // El problema es que si queremos ordenar el codigo por funciones no se puede hacer
+// })();
+
+//import te permite traerlo a este aechivo 
+import { nombreCliente, ahorro } from "./cliente.js";
+
+console.log(nombreCliente);
+console.log(ahorro);
+
+
+
---
+++ 28-Modulos/js/cliente.js
@@ -0,0 +1,4 @@
+// Para tener este valor disponible en otro archivo
+// Saca una variable, una clase, objeto una funcion lo que sea
+export const nombreCliente = 'Fernando';
+export const ahorro = 200;
