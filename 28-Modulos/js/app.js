bb9eed3d00e48b6a964be81406736ea6b9899949
Fernando Cuatro <fernandocuatrorivera@gmail.com>
2024-02-28 18:17:31 -0600

    Exportar e importar una clase con module

--- 28-Modulos/js/app.js
+++ 28-Modulos/js/app.js
@@ -5,10 +5,17 @@
 // })();
 
 //import te permite traerlo a este aechivo 
-import { nombreCliente, ahorro } from "./cliente.js";
+import { nombreCliente, ahorro, mostrarInformacion, tieneSaldo, Cliente } from "./cliente.js";
 
+// Variables 
 console.log(nombreCliente);
 console.log(ahorro);
 
+// Funciones
+console.log( mostrarInformacion( nombreCliente, ahorro ) );
+tieneSaldo( ahorro )
 
+// Clases 
+const cliente = new Cliente( nombreCliente, ahorro );
+console.log(cliente);
 
--- 28-Modulos/js/cliente.js
+++ 28-Modulos/js/cliente.js
@@ -2,3 +2,29 @@
 // Saca una variable, una clase, objeto una funcion lo que sea
 export const nombreCliente = 'Fernando';
 export const ahorro = 200;
+
+// Ojo con el export
+export function mostrarInformacion(nombre, ahorro) {
+    return `Cliente: ${nombre} - Ahorro: ${ahorro}`;
+}
+
+// Seguimos exportando funciones
+export function tieneSaldo( ahorro ) {
+    if ( ahorro > 0 ) {
+        console.log('Si tiene saldo');
+    } else {
+        console.log('El cliente no tiene saldo');
+    }
+}
+
+// Ahora exportamos una funcion
+export class Cliente {
+    constructor(nombre, ahorro) {
+        this.nombre = nombre;
+        this.ahorro = ahorro;
+    }
+
+    mostrarInformacion() {
+        return `Cliente: ${this.nombre} - Ahorro: ${this.ahorro}`;
+    }
+}
